0f60abe2a217dd66228b680fb011798e97c36c7c
anthonyzhu132 <anthonyzhu132@gmail.com>
2019-12-03 19:16:17 -0800

    implemented simple express erver as well as nodemon

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const express = require('express')
 const app = express()
+const port = 8080
+
+// Rendering Root Page
+app.get("/", (req, res) => {
+  res.send("Hello from the root page");
+});
+
+//Listening to Port 8080
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`)
+});
