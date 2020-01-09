5855ec191a9679f7d7f4d6f81b5107a40b90fc14
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-01-09 12:09:52 -0800

    refactored to use object deconstruction

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const postController = require('../controllers/post')
+const {getPosts, createPosts} = require('../controllers/post')
 const router = express.Router()
 const Validator = require('../Valdation');
+
 //Loading root page and getting posts
-router.get('/', postController.getPosts)
+router.get('/', getPosts)
 
 //Posting post information from frontend to backend
-router.post('/post', Validator.createsPostValidator, postController.createPosts)
+router.post('/post', Validator.createsPostValidator, createPosts)
 
 module.exports = router;
