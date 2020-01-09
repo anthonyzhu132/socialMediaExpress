d8a11271cc53257f8c1e7d2a5f67a51246ab2925
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-01-09 13:10:53 -0800

    added uuid package and encryption process for passwords to hash" "

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,5 +24,37 @@ const userSchema = new mongoose.Schema({
   },
   updated: Date
 });
+
+
+//Creating the hasehd passwords
+userSchema.virtual('password')
+.set(function() {
+  //Create a temp password from the one inputed
+  this._password = password
+  //generate a timestamp
+  this.salt = uuidv1()
+  //encrypt password using a function
+  this.hashed_password = this.encryptPassword(password)
+
+})
+.get(function() {
+  return this._password
+})
+
+//methods for the userSchema
+userSchema.methods = {
+  encryptPassword: function(password) {
+    if(!password) return "";
+    try {
+      //use encryption sha1 using the salt that was provided
+      return crypto.createHmac('sha1', this.salt)
+      //update password
+      .update(password)
+      .digest('hex');
+    } catch (err){
+      return "";
+    }
+  }
+}
 
 module.exports = mongoose.model("User", userSchema);
