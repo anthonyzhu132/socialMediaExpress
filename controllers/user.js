89e0ab4158fd55fcf074320051f95583f3c41428
Anthony Zhu <anthonyzhu132@gmail.com>
2020-02-10 23:31:50 -0800

    added updateUser method to allow users to update user profiles

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const _ = require('lodash')
 
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -36,4 +37,20 @@ exports.getuser = (req, res) => {
     req.profile.hashed_password = undefined;
     req.profile.salt = undefined;
     return res.json(req.profile);
+}
+
+exports.updateUser = (req, res, next) => {
+    let user = req.profile
+    user = _.extend(user, req.body) // extend method will take the user object and edit with the request body for any changes made
+    user.updated = Date.now();
+    user.save((err) => {
+        if(err) {
+            return res.status(400).json({
+                error: "You are not authorized to perform this action, please try again and check."
+            })
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json({user})
+    });
 }
