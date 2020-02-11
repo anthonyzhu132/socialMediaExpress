9a3ec822a67a76fc95ea148c3adcf7b8c2312364
Anthony Zhu <anthonyzhu132@gmail.com>
2020-02-10 23:33:25 -0800

    added put route to update user profile

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const { userById, allUsers, getuser } = require('../controllers/user')
+const { userById, allUsers, getuser, updateUser } = require('../controllers/user')
 const { requireSignin } = require('../controllers/auth')
 const router = express.Router()
 
 //adding a route to fetch all of the users
 router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getuser);
+router.put('/user/:userId', requireSignin, updateUser); // To update, use PUT method
 
 // any routes containing :userId, app will execute userbyid()
 router.param("userId", userById)
