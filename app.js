36ad841e0f44a739b1ddd974cc644f142c59cb8e
Anthony Zhu <anthonyzhu132@gmail.com>
2020-02-24 23:51:47 -0800

    git status

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 dotenv.config()
 
 
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true
+    .connect(process.env.MONGO_URI, {   
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
     .then(() => console.log('DB Connected'));
 
